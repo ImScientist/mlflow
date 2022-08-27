b170a7af098b4df1d8a27aca51250c7964fca50d
Anton Ivanov <a.i.ivanov.sv@gmail.com>
2022-08-27 17:21:57 +0200

    fix terrafrom kubernetes cluster creation and gcr pull rights

--- terraform/README.md
+++ terraform/README.md
@@ -1,7 +1,19 @@
 ### Create infrastructure with terraform (WIP)
 
+- THings to automate:
+  - setup password of the default `postgres` user: at the moment, we change them manually and terraform does not detect any state change.
+  
 - Install the terraform version manager. We will work with version `1.2.7`: 
   ```shell
   tfenv install 1.2.7
   tfenv use 1.2.7
+  ```
+
+- Set the `project`, `region` and `zone` in `variables.tf` and authenticate:
+  ```shell
+  gcloud init
+  gcloud auth application-default login
+  
+  terraform init
+  terraform fmt
   ```
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,8 @@ provider "google" {
   zone    = var.zone
 }
 
+data "google_client_config" "default" {}
+
 resource "google_project_service" "iam" {
   for_each           = toset(["iam.googleapis.com", "container.googleapis.com", "cloudbuild.googleapis.com", "containerregistry.googleapis.com", "sqladmin.googleapis.com"])
   project            = var.project
@@ -13,11 +15,17 @@ resource "google_project_service" "iam" {
 
 module "artifacts" {
   source   = "./modules/storage"
-  name     = "artifacts-4238717282752847c3d58e99"
+  name     = "artifacts-4238717282752847c3d58e44"
   location = var.region
   tags = {
     Desc = "mlflow artifact storage"
   }
+}
+
+// terraform import google_storage_bucket.ai_whatever_imported ai-whatever
+resource "google_storage_bucket" "ai_whatever_imported" {
+  name          = "ai-whatever"
+  location      = "europe-west3"
 }
 
 module "mlflow_backend" {
@@ -68,37 +76,69 @@ resource "google_project_iam_member" "access_gcr" {
   depends_on = [module.account_gcr]
 }
 
-//resource "kubernetes_secret" "secret_mlflow" {
-//  metadata {
-//    name      = "mlflow-secret"
-//    namespace = "mlflow"
-//
-//    labels = {
-//      source = "terraform"
-//    }
-//  }
-//
-//  data = {
-//    "csql-auth" = module.account_csql.private_key_decoded
-//    "gcs-auth"  = module.account_gcs.private_key_decoded
-//    "sql_usr"   = "postgres"
-//    "sql_db"    = "postgres"
-//  }
-//}
+resource "google_container_cluster" "primary" {
+  name                     = "gcc-ai"
+  location                 = var.zone  // pick region or zone
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
 
-//resource "kubernetes_secret" "gcr-io-secret" {
-//  metadata {
-//    name      = "gcr-io-secret"
-//    namespace = "mlflow"
-//
-//    labels = {
-//      source = "terraform"
-//    }
-//  }
-//
-//  data = {
-//    "docker-server"   = "gcr.io"
-//    "docker-username" = "_json_key"
-//    "docker-password" = module.account_gcr.private_key_decoded
-//  }
-//}
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "primary-node-pool"
+  location   = var.zone  // pick region or zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 3
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+    disk_size_gb = 20
+    disk_type    = "pd-standard"
+
+    // This service account has only read access to GCR
+    service_account = module.account_gcr.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+  }
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.primary.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+}
+
+resource "kubernetes_namespace" "mlflow" {
+  metadata {
+    name   = "mlflow"
+
+    labels = {
+      mylabel = "label-mlflow-namespace"
+    }
+
+  }
+}
+
+resource "kubernetes_secret" "secret_mlflow" {
+  metadata {
+    name      = "mlflow-secret"
+    namespace = "mlflow"
+
+    labels = {
+      source = "terraform"
+    }
+  }
+
+  data = {
+    "csql-auth" = module.account_csql.private_key_decoded
+    "gcs-auth"  = module.account_gcs.private_key_decoded
+    "sql_usr"   = "postgres"
+    "sql_pwd"   = "13ce5bad0e284ab512b928ea"
+    "sql_db"    = "postgres"
+  }
+
+  type = "Opaque"
+  depends_on = [module.account_gcs]
+}
